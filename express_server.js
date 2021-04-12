733649454e07acfb215e755cfc8cdf87779d0eb7
A <awongpro7@gmail.com>
2021-04-12 23:18:47 +0000

    Added morgan

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+morgan = require('morgan');
+app.use(morgan('dev'));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
