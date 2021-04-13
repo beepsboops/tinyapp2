fb62f0cc4d19ae6843a29aa6aa0472e4a6dcd98c
A <awongpro7@gmail.com>
2021-04-13 03:09:04 +0000

    Implement random Short URL function

--- express_server.js
+++ express_server.js
@@ -7,10 +7,29 @@ app.set("view engine", "ejs");
 morgan = require('morgan');
 app.use(morgan('dev'));
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+  ///////////////
+ // DATABASE //
+/////////////
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+  ///////////////////////////////
+ // RANDOM SHORT URL FUNCTION //
+///////////////////////////////
+
+const randomShortURL = function(length=6){
+  return Math.random().toString(20).substr(2, length)
+  }
+
+  /////////////////
+ // GET ROUTES //
+///////////////
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -28,12 +47,28 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// GET // => MY URLS PAGE
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// GET // => CREATE NEW URL/TINYURL PAGE
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// GET // => NEW SHORT URL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+  //////////////////
+ // POST ROUTES //
+////////////////
+
+app.post("/urls", (req, res) => {
+  shortURL = randomShortURL()
+  res.redirect(`/urls/${shortURL}`);
 });
